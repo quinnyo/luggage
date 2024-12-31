ced80f4f0559c8a43ee84a04326f10e39b287036
Quinn <3379314+quinnyo@users.noreply.github.com>
2024-12-31 13:46:54 +1000

    remove type_id from ZedClass

--- bench/zed/zed_class.gd
+++ bench/zed/zed_class.gd
@@ -5,22 +5,15 @@ extends RefCounted
 const TYPE_INFO_UID := &"uid"
 const TYPE_INFO_PATH := &"path"
 const TYPE_INFO_GLOBAL_NAME := &"global_name"
-const TYPE_INFO_TYPE_ID := &"type_id"
 const TYPE_INFO_TYPE_NAME := &"type_name"
 
 
-var _type_id: int
 var _type_name: StringName
 var _type_info: Dictionary[StringName, Variant]
 
 
 func get_type_info() -> Dictionary[StringName, Variant]:
 	return _type_info.duplicate()
-
-
-## Get this ZedClass's unique identifier.
-func get_type_id() -> int:
-	return _type_id
 
 
 ## Get this ZedClass's unique identifier.
@@ -39,13 +32,6 @@ func serialise(inst: Node) -> Variant:
 
 func deserialise(inst: Node, data) -> Error:
 	return _zed_deserialise(inst, data)
-
-
-## The base impl returns the resource UID of the script. If needed, a custom
-## [b]unique[/b] identifier can be provided by overriding this method.
-func _zed_get_type_id() -> int:
-	var s: Script = get_script()
-	return ResourceLoader.get_resource_uid(s.resource_path)
 
 
 ## The base impl returns the resource UID of the attached script.
@@ -73,14 +59,12 @@ func _zed_deserialise(inst: Node, data) -> Error:
 
 
 func _init() -> void:
-	_type_id = _zed_get_type_id()
 	_type_name = _zed_get_type_name()
 	var s: Script = get_script()
 	_type_info = {
 		TYPE_INFO_UID: ResourceLoader.get_resource_uid(s.resource_path),
 		TYPE_INFO_PATH: s.resource_path,
 		TYPE_INFO_GLOBAL_NAME: s.get_global_name(),
-		#TYPE_INFO_TYPE_ID: _zed_get_type_id(),
 		TYPE_INFO_TYPE_NAME: _zed_get_type_name(),
 	}
 
