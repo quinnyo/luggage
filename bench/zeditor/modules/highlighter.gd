057758d0dac7468fbf508b43797d764125416bf9
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-01-22 09:18:03 +1000

    Highlighter: add own function to find part placement node

--- bench/zeditor/modules/highlighter.gd
+++ bench/zeditor/modules/highlighter.gd
@@ -12,6 +12,15 @@ class Highlight:
 var _editables: Dictionary[int, Highlight]
 
 
+func _get_placement(editable_id: int) -> Node3D:
+	var zhost := get_bench().get_zed_host()
+	var shell_id := zhost.get_part_shell_id(editable_id)
+	for node in get_bench().get_workspace().shell_get_nodes(shell_id):
+		if node is Node3D:
+			return node as Node3D
+	return null
+
+
 func _module_registered(id: int, zeditor: Zeditor) -> void:
 	zeditor.module_activate(id, zeditor.channel_alloc())
 
@@ -31,6 +40,6 @@ func _module_on_zeditor_editable_deactivated(editable_id: int) -> void:
 
 func _process(_delta: float) -> void:
 	for editable_id in _editables:
-		var placement := _zeditor.get_active_placement(editable_id)
+		var placement := _get_placement(editable_id)
 		var highlight := _editables[editable_id]
 		highlight.pointer.global_position = placement.global_position
