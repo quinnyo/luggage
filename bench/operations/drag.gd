ddc34394348c5912a1b86e7276ab4bd5a104d895
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-02-09 02:57:13 +1000

    Add 'tool.move' and 'op.drag'
    - test tool & operation from hello.gd

---
+++ bench/operations/drag.gd
@@ -0,0 +1,49 @@
+extends ZedOperation
+
+
+var _zhost: ZedHost
+var _workspace: Workspace
+var _pointer_start: Vector3
+var _drag_delta: Vector3
+var _target_setter: Array[Callable]
+var _target_initial_value: PackedVector3Array
+
+
+## Add a target object to the operation.
+## [param initial_value] is the object's current position.
+## [param fset] a ([code]func(Vector3) -> void[/code]) that sets the target object's position.
+func add_target(initial_value: Vector3, fset: Callable) -> void:
+	assert(get_bind_status() == BindStatus.UNBOUND)
+	_target_initial_value.push_back(initial_value)
+	_target_setter.push_back(fset)
+
+
+func _bind(bench: Bench) -> BindStatus:
+	_zhost = bench.get_zed_host()
+	_workspace = bench.get_workspace()
+	_pointer_start = _workspace.pointer_3d_get_position()
+	return BindStatus.OK
+
+
+func _get_name() -> String:
+	return "op.drag"
+
+
+func _do() -> void:
+	for i in range(_target_setter.size()):
+		var initial_value := _target_initial_value[i]
+		var new_value := initial_value + _drag_delta
+		_target_setter[i].call(new_value)
+
+
+func _undo() -> void:
+	for i in range(_target_setter.size()):
+		_target_setter[i].call(_target_initial_value[i])
+
+
+func _is_interactive() -> bool:
+	return true
+
+
+func _interactive_update() -> void:
+	_drag_delta = _workspace.pointer_3d_get_position() - _pointer_start
---
+++ bench/zeditor/tools/move.gd
@@ -0,0 +1,29 @@
+extends ZeditorTool
+
+
+func _tool_get_name() -> String:
+	return "tool.move"
+
+
+func _tool_can_activate(context: Context) -> bool:
+	return context.selection.is_empty() || context.selection.has_item_type(Zed.ItemType.ARMATURE_POINT)
+
+
+func _tool_activate(context: Context) -> void:
+	var op_drag := preload("res://bench/operations/drag.gd").new()
+	var selected_armature := context.selection.get_selectables_with_item_type(Zed.ItemType.ARMATURE_POINT)
+	if selected_armature.size():
+		var zhost := context.bench.get_zed_host()
+		for sel in selected_armature:
+			var part_id: int = sel.indexed_get_item()
+			var points := sel.indexed_get_indices()
+			for i in range(points.size()):
+				var index := points[i]
+				var initial_position := zhost.part_armature_get_transform(part_id, index).origin
+				op_drag.add_target(initial_position, func(value: Vector3):
+					var xf := zhost.part_armature_get_transform(part_id, index)
+					xf.origin = value
+					zhost.part_armature_set_transform(part_id, index, xf)
+				)
+
+		context.operation_begin(op_drag)
