dec932d3549f87f7ffac441bbef0b3ac1ee03633
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-04-03 11:53:56 +1000

    remove redundant picking impl from workspace

--- bench/workspace/workspace.gd
+++ bench/workspace/workspace.gd
@@ -31,9 +31,6 @@ signal part_support_removed(part_id: int, pair: int, count: int)
 signal part_supported(part_id: int)
 signal part_unsupported(part_id: int)
 
-signal pointer_entered_part(part_id: int)
-signal pointer_exited_part(part_id: int)
-
 signal pointer_3d_entered(pos: Vector3)
 signal pointer_3d_exited()
 signal pointer_3d_moved(pos: Vector3, delta: Vector3)
@@ -41,8 +38,6 @@ signal pointer_3d_moved(pos: Vector3, delta: Vector3)
 
 var _pointer_3d_position: Vector3
 var _pointer_3d_is_present: bool = false
-var _picked: Node
-var _picked_volume: int
 var _layers: Array[_WorkspaceShellLayer]
 var _placement_conflicts: Dictionary[int, Dictionary]
 var _volumes: VolumeManager = VolumeManager.new()
@@ -221,24 +216,6 @@ func pointer_3d_is_present() -> bool:
 	return _pointer_3d_is_present
 
 
-func get_picked_node() -> Node:
-	return _picked
-
-
-func is_part_picked() -> bool:
-	return _placement_volumes.has(_picked_volume)
-
-
-func get_picked_placement() -> int:
-	return _picked_volume if _placement_volumes.has(_picked_volume) else VolumeManager.ID_NONE
-
-
-func get_picked_part() -> int:
-	if is_part_picked():
-		return _placement_volumes[_picked_volume][K_PLACEMENT_PART_ID]
-	return 0
-
-
 func _shell_stack_is_empty() -> bool:
 	return _layers.is_empty()
 
@@ -259,30 +236,6 @@ func _notify_shell_disappearing(layer: _WorkspaceShellLayer) -> void:
 
 func _support_is_occupied(support_volume_id: int) -> bool:
 	return _support_volumes[support_volume_id][K_SUPPORT_CLIENTS].size()
-
-
-func _set_picked(object: Node) -> void:
-	if _picked == object:
-		return
-
-	if _picked:
-		if is_part_picked():
-			pointer_exited_part.emit(get_picked_part())
-		_picked = null
-		_picked_volume = VolumeManager.ID_NONE
-
-	if object && is_ancestor_of(object) && object is Area3D:
-		_picked_volume = _volumes.find_volume_with_area(object)
-		if _picked_volume != VolumeManager.ID_NONE:
-			_picked = object
-			if is_part_picked():
-				pointer_entered_part.emit(get_picked_part())
-
-
-func _process(_delta: float) -> void:
-	var vpp := ViewportPlus.get_viewport_plus(self)
-	if vpp.get_picking().get_object() is Node:
-		_set_picked(vpp.get_picking().get_object())
 
 
 func _init() -> void:
